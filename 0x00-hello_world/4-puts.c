46d808871a308052a7607c422584576043d649a4
Maryswy <marysowa54@gmail.com>
2022-09-10 13:14:49 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-This is the main function
+ * Description:prints out a string
+ * Return:Always 0 (successs)
+ */
+
+int main(void)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
