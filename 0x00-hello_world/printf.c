8ea139108ef9f0c8766689451f0203f03bba1ad7
Maryswy <marysowa54@gmail.com>
2022-09-10 13:43:26 -0700

    printf

---
+++ 0x00-hello_world/printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main-This is the main function
+ * Description:Prints a sentence with a new line
+ * Return 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar,but the outcome is a piece of art,\n");
+	return (0);
+}
