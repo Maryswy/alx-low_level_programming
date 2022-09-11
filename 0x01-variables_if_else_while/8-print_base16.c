af505aaf9c6b9d43d546063078dd6fb94ac13c29
Maryswy <marysowa54@gmail.com>
2022-09-11 11:56:13 -0700

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers of base 16 in lowercase.Only use putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 48;
+
+	int 1 = 'a';
+	while (n <= 57)
+	{
+		putchar(n);
+		n += 1;
+	}
+	while (1 <= 'f')
+	{
+		putchar(1);
+		1++;
+	}
+	putchar('\n');
+	return (0);
+}
