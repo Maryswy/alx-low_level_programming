9b6919cb2996251461ff8ee7893a43a2f94264d8
Maryswy <marysowa54@gmail.com>
2022-09-10 19:11:18 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main-main block
+ * Description:Assign a random number and print the last digit
+ * Return:Always 0 (success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	putchar (c) c++;
+
+	while (c <= 'z')
+		put char ('\n');
+	return (0);
+}
