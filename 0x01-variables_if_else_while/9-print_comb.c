cffd2684d28d19646495adbdfb4be0f16b380a69
Maryswy <marysowa54@gmail.com>
2022-09-11 16:48:13 -0700

    number combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
@@ -9,17 +7,17 @@
 
 int main(void)
 {
-	int d;
+	int n;
 
-	for (d = '0'; d <= '9'; d++)
+	for (n = 48; n < 58; n++)
 	{
-		putchar(d);
-		if (d != '9')
+		putchar(n);
+		if (n != 57)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-		putchar('\n');
-		return (0);
 	}
+	putchar('\n');
+	return (0);
 }
