99c8f8627cbf3298090f146a0f2f29466e89b6f2
Maryswy <marysowa54@gmail.com>
2022-09-12 08:47:46 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 0;
+	char str_hbt[9] = "_putchar";
+
+	while (n < 9)
+	{
+		_putchar(str_hbt[n]);
+		n += 1;
+	}
+	_putchar('\n');
+	return (0);
+}
