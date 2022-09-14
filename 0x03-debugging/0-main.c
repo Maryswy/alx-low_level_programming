62b312e71070ae7b3cf3c26ffc9b193b5a840187
Maryswy <marysowa54@gmail.com>
2022-09-14 08:42:12 -0700

    main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,14 @@
 
 /**
  * main - a test function that prints positive or negative integer
- * Return:0
+ * Return: 0
  */
 
 int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
 	return (0);
 }
