064cec5595493c9efe149d5c304fe8b3b2d0b440
Maryswy <marysowa54@gmail.com>
2022-10-03 03:43:04 -0700

    an array of chars

---
+++ 0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - main entry
+ * @size: size input
+ * @c: char
+ * Return: Always 0 (success)
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	array = malloc(sizeof(char) * size);
+
+	if (size == 0 || array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+	return (array);
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-My readme for hello world
+Concepts
+For this project, we expect you to look at this concept:
+
+Automatic and dynamic allocation, malloc and free
+
+
+
+General
+What is the difference between automatic and dynamic allocation
+What is malloc and free and how to use them
+Why and when use malloc
+How to use valgrind to check for memory leak
+
